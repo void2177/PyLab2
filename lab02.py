cb6397d8ee70c42f1396f3c94412d6376564689b
carterbrody <clb2377@gmail.com>
2025-09-03 11:54:46 -0700

    Finished Lab 2, additionally created a dialogue menu wherein you can choose to convert between celsius or fahrenheit, with degree icons as well.

--- lab02.py
+++ lab02.py
@@ -49,5 +49,21 @@ print(f"""
 {age ** 2},
 """)
 import math
-temperature = input("What fahrenheit degree temperature would you like to convert to celsius?")
-print(f"{temperature - 32 * (5/9)}")
+##temperature = float(input("What fahrenheit degree temperature would you like to convert to celsius?"))
+##celsius = (temperature - 32) * (5/9)
+##print(f"{temperature}°F is {celsius:.2f}°C")
+
+## test
+format = input("Would you like to convert to celsius or fahrenheit?")
+if format == "fahrenheit":
+    temperature = float(input("What fahrenheit degree temperature would you like to convert to celsius?"))
+    celsius = (temperature - 32) * (5/9)
+    print(f"{temperature}°F is {celsius:.2f}°C")
+
+elif format == "celsius":
+    temperature = float(input("What celsius degree temperature would you like to convert to fahrenheit?"))
+    fahrenheit = (temperature * 9/5) + 32
+    print(f"{temperature}°C is {fahrenheit:.2f}°F")
+
+else:
+    print("Please enter a valid input.")
